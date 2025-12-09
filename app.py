545a1831f1e9d03db76a8795b6a38c6f3d2b95d2
Sai Arun Mendu <smendu@constructor.university>
2025-12-09 21:09:04 +0100

    updated rag

--- app.py
+++ app.py
@@ -16,8 +16,9 @@ from pathlib import Path
 import sys
 
 # Make sure Python can find your src/ package
-sys.path.append(str(Path(__file__).parent))
-
+ROOT_DIR = Path(__file__).resolve().parent  # folder containing app.py
+if str(ROOT_DIR) not in sys.path:
+    sys.path.insert(0, str(ROOT_DIR))
 from rag.src.config.config import Config
 from rag.src.document_ingestion.document_processor import DocumentProcessor
 from rag.src.vectorstore.vectorstore import VectorStore
